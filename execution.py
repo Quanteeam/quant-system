4a97f4b313494b8c19e2098fd90c85eeca104ada
thdalswo4524 <thdalswo4524@gmail.com>
2026-05-11 14:44:36 +0900

    Remove out-of-scope root shims

--- execution.py
+++
@@ -1,8 +0,0 @@
-from importlib import import_module as _import_module
-import sys as _sys
-
-_impl = _import_module("trading.execution")
-for _name, _value in vars(_impl).items():
-    if not _name.startswith("__"):
-        globals()[_name] = _value
-_sys.modules[__name__] = _impl
